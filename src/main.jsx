0794adb2ba1ae02ee922212e4d8f927ef55a4b5b
Chris Whong <chris.m.whong@gmail.com>
2023-05-22 14:37:54 -0400

    prep for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/chronosubway">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
   // </React.StrictMode>,
